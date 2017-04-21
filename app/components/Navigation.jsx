a2866c4bb8226ef39fb76425bdd7dcd14aff4d2c
Simon Conway <simon.conway@capgemini.com>
2017-04-21 14:45:29 +0100

    Added and styled the navigation bar

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer</li>
+            <li><IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
+            <li><Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://www.linkedin.com/in/simonconway/">Simon Conway</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  };
+
+module.exports = Navigation
