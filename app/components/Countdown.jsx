7e15ef705ad346a038b811fc88fa5437a61b5a3f
Simon Conway <simon.conway@capgemini.com>
2017-04-26 09:54:15 +0100

    Finalized tests and added page title

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -78,6 +78,7 @@ var Countdown = React.createClass({
 
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
