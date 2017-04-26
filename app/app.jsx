5f40776ea32428df66c27be5595e2a1db0aa7fca
Simon Conway <simon.conway@capgemini.com>
2017-04-26 10:54:05 +0100

    Added the clock and controls to the timer

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,7 @@ var Main = require('Main');
 var Timer = require('Timer');
 var Countdown = require('Countdown');
 
+
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,37 @@
 var React = require('react');
+var Controls = require('Controls');
+var Clock = require('Clock');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+        count: 0,
+        timerStatus: 'stopped'
+    };
+  },
+
+  // componentDidUpdate: function (prevProps, prevState) {
+  //   if (this.state.timerStatus !== prevState.timerStatus) {
+  //     switch (this.state.timerStatus) {
+  //       case 'started':
+  //         this.startTimer();
+  //
+  //     }
+  //   }
+  // }
+
+  handleStatusChange: function (newTimerStatus) {
+    console.log('newTimerStatus');
+  },
+
   render: function () {
+    var {count, timerStatus} = this.state;
+
     return(
       <div>
-        <h3>Timer component</h3>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     )
   }
